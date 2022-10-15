58338f7d1db108dcf57d863a8e8d8f8bf76e5d93
isaiasaraujosilva <isaiasaraujosilva>
2022-10-15 18:34:39 -0300

    feat: add table in page "adminPanelAllUser"

--- src/Pages/adminPanelAllUser.php
+++ src/Pages/adminPanelAllUser.php
@@ -40,14 +40,14 @@ $user = $_SESSION['user'];
             <td>Data de Nascimento</td>
             <td>Genero</td>
         </tr>
-    <?php foreach ($peoples as $people) { ?>
+    <?php foreach ($people as $people):?>
         <tr>
-            
-            <td><?=$people['name']?></td>
-            <td></td>
-            <td></td>
+            <td><?= $people['name']?></td>
+            <td><?= $people['birth_date']?></td>
+            <td><?= $people['gender']?></td>
+        </tr>
 
-    <?php } ?>
+    <?php endforeach; ?>
 <?php
     include_once '../Pages/elements/footer.php';
 ?>
