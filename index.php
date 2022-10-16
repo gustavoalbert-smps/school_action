b5668c20a1ee175911bc84703140c82fed20aa0e
isaiasaraujosilva <isaiasaraujosilva>
2022-10-15 23:28:07 -0300

    fix: redirect index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<head>
+    <meta charset="utf-8"/>
+    <title>Meu Redirect</title>
+
+    <meta http-equiv="refresh" content="5; URL='Pages/index.php'"/>
+</head>
+</head>
+<body>
+</body>
+</html>
