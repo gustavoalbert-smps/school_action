b98c8a5060500a76112eefd877fc12348540fc99
isaiasaraujosilva <isaiasaraujosilva>
2022-10-24 20:10:13 -0300

    feat: add new attribute for class photo

--- src/Domain/Model/Photo.php
+++ src/Domain/Model/Photo.php
@@ -5,17 +5,32 @@ class Photo
 {
 
     private int $id;
+    private string $name;
+    private string $type;
     private string $path;
     private int $people_id;
     
-    public function __construct(int $id, string $path, int $people_id)
+    public function __construct(int $id,string $name,string $type, string $path, int $people_id)
     {
         $this->id = $id;
+        $this->name = $name;
+        $this->type = $type;
         $this->path = $path;
         $this->people_id = $people_id;
+        
     }
     
 //getters
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+    
     public function getId()
     {
         return $this->id;
