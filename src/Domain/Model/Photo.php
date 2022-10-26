7828f3ef448596a1cf7570024130ae1127c53f9d
isaiasaraujosilva <isaiasaraujosilva>
2022-10-26 15:55:23 -0300

    remov: attribute 'path'

--- src/Domain/Model/Photo.php
+++ src/Domain/Model/Photo.php
@@ -7,40 +7,30 @@ class Photo
     private int $id;
     private string $name;
     private string $type;
-    private string $path;
     private int $people_id;
     
-    public function __construct(int $id,string $name,string $type, string $path, int $people_id)
+    public function __construct(int $id,string $name,string $type, int $people_id)
     {
         $this->id = $id;
         $this->name = $name;
         $this->type = $type;
-        $this->path = $path;
         $this->people_id = $people_id;
         
     }
     
 //getters
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getType()
-    {
-        return $this->type;
-    }
-    
     public function getId()
     {
         return $this->id;
     }
-
-    public function getPath()
+    public function getName()
     {
-        return $this->path;
+        return $this->name;
     }
-
+    public function getType()
+    {
+        return $this->type;
+    }
     public function getPeople_id()
     {
         return $this->people_id;
