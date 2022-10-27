71e8c5bbbdfd54be0a5f85e1d90bfceb65c17e12
gustavo <gustavoads.millionary@gmail.com>
2022-10-27 12:23:23 -0300

    feature: insertStudent function

--- src/Infrastructure/Controller/StudentController.php
+++ src/Infrastructure/Controller/StudentController.php
@@ -2,7 +2,9 @@
 
 namespace Alura\Pdo\Infrastructure\Controller;
 
+use Alura\Pdo\Domain\Model\Student;
 use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+use DateTimeImmutable;
 use PDO;
 
 require_once '../../vendor/autoload.php';
@@ -19,5 +21,14 @@ class StudentController
     public function numberOfStudentsByClass(PdoStudentRepository $studentRepository, int $classId): int
     {
         return $studentRepository->countStudentsByClass($classId);
+    }
+
+    public function insertStudent(PdoStudentRepository $studentRepository, int $peopleId, string $name, string $gender, string $birthDate, int $classId, int $admin): Student
+    {
+        $student = new Student(null, $peopleId, $name, $gender, new DateTimeImmutable($birthDate), $classId, $admin);
+
+        $studentRepository->save($student);
+
+        return $student;
     }
 }
