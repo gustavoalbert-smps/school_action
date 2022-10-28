f7d87f323f2d4f52124b665b18eebec105598b85
Gustavo Albert <59424110+gustavoalbert-smps@users.noreply.github.com>
2022-10-28 12:15:05 -0300

    Feature/cascade removing student (#11)
    * update: student attributes
    
    * feature: removePeople function
    
    * feature: getUserWithPeopleId and removeUser functions
    
    * feature: cascade removing student
    
    * update: student attributes on the schoolClass.php page

--- src/Pages/removeStudent.php
+++ src/Pages/removeStudent.php
@@ -1,8 +1,12 @@
-<?php 
+<?php
 
-use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Controller\PeopleController;
+use Alura\Pdo\Infrastructure\Controller\StudentController;
+use Alura\Pdo\Infrastructure\Controller\UserController;
 use Alura\Pdo\Infrastructure\Persistence\ConnectDatabase;
+use Alura\Pdo\Infrastructure\Repository\PdoPeopleRepository;
 use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+use Alura\Pdo\Infrastructure\Repository\PdoUserRepository;
 
 require_once '../../vendor/autoload.php';
 
@@ -12,34 +16,51 @@ if (empty($_SESSION['user']) || empty($_SESSION['password'])) {
     $_SESSION = array();
     header('Location: /pdo/src/Pages/index.php');
 } else {
-
     $connection = ConnectDatabase::connect();
-    $repository = new PdoStudentRepository($connection);
+
+    $studentRepository = new PdoStudentRepository($connection);
+    $studentController = new StudentController($connection);
+
+    $userRepository = new PdoUserRepository($connection);
+    $userController = new UserController($connection);
+
+    $peopleRepository = new PdoPeopleRepository($connection);
+    $peopleController = new PeopleController($connection);
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $student = $repository->getStudent(intval($_POST['id']));
-        $repository->remove($student);
-        header('Location: studentsModule.php');
+        $student = $studentController->getStudent($studentRepository, intval($_POST['id']));
+        
+        $user = $userController->getUserWithPeopleId($userRepository, $student->getPeopleId());
+
+        $people = $peopleController->getPeople($peopleRepository, $student->getPeopleId());
+
+        if ($studentController->removeStudent($studentRepository, $student) && 
+            $userController->removeUser($userRepository, $user) && 
+            $peopleController->removePeople($peopleRepository, $people)){
+            header('Location: studentsModule.php');
+        } else {
+            echo 'erro';
+        }
     } else {
-        $student = $repository->getStudent(intval($_GET['id']));
+        $student = $studentController->getStudent($studentRepository, intval($_GET['id']));
     }
 
     include_once '../Pages/elements/head.php';
 ?>
 
 
-        <div class="container">
-            <h1><?php echo $student->getName()?></h1>
-            <h4><?php echo $student->getBirthDate()->format('d-m-Y')?></h4>
-            <p><?php echo $student->getClassId()?></p>
-        </div>
-        <div id="confirmation-box">
-            <form action="removeStudent.php" method="POST">
-                <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
-                <button class="btn">Confirmar</button>
-            </form>
-            <button class="btn" onclick="window.location.href = 'studentsModule.php'">Cancelar</button>
-        </div>
+    <div class="container">
+        <h1><?php echo $student->getName()?></h1>
+        <h4><?php echo $student->getBirthDate()->format('d-m-Y')?></h4>
+        <p><?php echo $student->getClassId()?></p>
+    </div>
+    <div id="confirmation-box">
+        <form action="removeStudent.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
+            <button class="btn">Confirmar</button>
+        </form>
+        <button class="btn" onclick="window.location.href = 'studentsModule.php'">Cancelar</button>
+    </div>
 
 <?php
     include_once '../Pages/elements/footer.php';
--- src/Pages/schoolClass.php
+++ src/Pages/schoolClass.php
@@ -12,7 +12,6 @@ if (empty($_SESSION['user']) || empty($_SESSION['password'])) {
     $_SESSION = array();
     header('Location: /pdo/src/Pages/index.php');
 } else {
-
     $connection = ConnectDatabase::connect();
 
     $classRepository = new PdoSchoolClassRepository($connection);
@@ -53,12 +52,12 @@ if (empty($_SESSION['user']) || empty($_SESSION['password'])) {
         <?php foreach ($students as $student) { ?>
             <tr>
                 <td class="student-tr name" align="center">
-                    <a class="student" href="student.php?id=<?php echo $student->id()?>"><?php echo $student->getName() ?></a>
+                    <a class="student" href="student.php?id=<?php echo $student->getId()?>"><?php echo $student->getName() ?></a>
                 </td>
                 <td class="student-tr" align="center"><?php echo $student->getBirthDate()->format('d-m-Y') ?></td>
                 <td class="student-tr" align="center"><?php echo $student->getClassId() ?></td>
                 <td class="student-tr" align="center">
-                    <button class="btn" id="x" onclick="window.location.href='/student/removeStudent.php?id=<?php echo $student->id()?>'">
+                    <button class="btn" id="x" onclick="window.location.href='removeStudent.php?id=<?php echo $student->getId();?>'">
                         <img class="x-icon" src="../Pages/imgs/x-icon.svg" alt="x-icon">
                     </button>
                 </td>
