4c709fb95e62cc90792a0c282b0316310341dc75
isaiasaraujosilva <isaiasaraujosilva>
2022-10-29 19:41:07 -0300

    style: change edit form

--- src/Pages/Profile.php
+++ src/Pages/Profile.php
@@ -132,15 +132,6 @@ if (empty($_SESSION['user']) || empty($_SESSION['password'])) {
                   <!-- Profile Edit Form -->
                   <form action="ProfileUpdate.php" method="POST" enctype="multipart/form-data">
                     <div class="row mb-3">
-                      <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">Imagem do perfil</label>
-                      <div class="col-md-8 col-lg-9">
-                        <img src="assets/img/messages-3.jpg" alt="Profile">
-                        <div class="pt-2">
-                          <input type="file" name="img" id="id_img">
-                          <a href="#" class="btn btn-primary btn-sm" title="Upload new profile image"><i class="bi bi-upload"></i></a>
-                          <a href="#" class="btn btn-danger btn-sm" title="Remove my profile image"><i class="bi bi-trash"></i></a>
-                        </div>
-                      </div>
                     </div>
 
                     <input type="hidden" name="id" value="<?=$id?>">
