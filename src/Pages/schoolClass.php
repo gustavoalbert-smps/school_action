306405d4155cc91d50946187bafd2128866f73d5
gustavo <gustavoads.millionary@gmail.com>
2022-10-31 10:05:44 -0300

    feature: x-button is only available for admin user

--- src/Pages/schoolClass.php
+++ src/Pages/schoolClass.php
@@ -23,52 +23,55 @@ if (empty($_SESSION['user']) || empty($_SESSION['password'])) {
     $students = [];
     $students += $studentRepository->getStudentsByClass(intval($_GET['id']));
 
-?>
-
-<?php
     require_once '../Pages/elements/head.php';
 ?>
 
-<div class="pagetitle">
-      <h1>Visualização de Turma</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="schoolClassModule.php">Turmas</a></li>
-          <li class="breadcrumb-item active"><?php echo "{$class->getYear()}{$class->getIdentifier()} ";?> <?php echo strtoupper($class->getShift());?></li>
-        </ol>
-      </nav>
-</div>
+    <div class="pagetitle">
+        <h1>Visualização de Turma</h1>
+        <nav>
+            <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="schoolClassModule.php">Turmas</a></li>
+            <li class="breadcrumb-item active"><?php echo "{$class->getYear()}{$class->getIdentifier()} ";?> <?php echo strtoupper($class->getShift());?></li>
+            </ol>
+        </nav>
+    </div>
 
-<div id="table-container">   
-    <table class="table table-striped students-table">
-        <thead>
-            <tr class="title">
-                <td class="head" align="center">Nome</td>
-                <td class="head" align="center">Data de Nascimento</td>
-                <td class="head" align="center">Classe Pertencente</td>
-                <td class="head"></td>
-            </tr>
-        </thead>
-        <?php foreach ($students as $student) { ?>
-            <tr>
-                <td class="student-tr name" align="center">
-                    <a class="student" href="student.php?id=<?php echo $student->getId()?>"><?php echo $student->getName() ?></a>
-                </td>
-                <td class="student-tr" align="center"><?php echo $student->getBirthDate()->format('d-m-Y') ?></td>
-                <td class="student-tr" align="center"><?php echo $student->getClassId() ?></td>
-                <td class="student-tr" align="center">
-                    <button class="btn" id="x" onclick="window.location.href='removeStudent.php?id=<?php echo $student->getId();?>'">
-                        <img class="x-icon" src="../Pages/imgs/x-icon.svg" alt="x-icon">
-                    </button>
-                </td>
-            </tr>
-    <?php } ?>
-    </table>
-</div>
-
-<div class="register-student">
-    <a href="registerStudent.php">Cadastrar Aluno</a>
-</div>
+    <div id="table-container">   
+        <table class="table table-striped students-table">
+            <thead>
+                <tr class="title">
+                    <td class="head" align="center">Nome</td>
+                    <td class="head" align="center">Data de Nascimento</td>
+                    <td class="head" align="center">Classe Pertencente</td>
+                    <?php if ($_SESSION['admin']) {?>
+                        <td class="head"></td>
+                    <?php }?>
+                </tr>
+            </thead>
+            <?php foreach ($students as $student) { ?>
+                <tr>
+                    <td class="student-tr name" align="center">
+                        <a class="student" href="student.php?id=<?php echo $student->getId()?>"><?php echo $student->getName() ?></a>
+                    </td>
+                    <td class="student-tr" align="center"><?php echo $student->getBirthDate()->format('d-m-Y') ?></td>
+                    <td class="student-tr" align="center"><?php echo $student->getClassId() ?></td>
+                    <?php if ($_SESSION['admin']) {?>
+                    <td class="student-tr" align="center">
+                        <button class="btn" id="x" onclick="window.location.href='removeStudent.php?id=<?php echo $student->getId();?>'">
+                            <img class="x-icon" src="../Pages/imgs/x-icon.svg" alt="x-icon">
+                        </button>
+                    </td>
+                    <?php }?>
+                </tr>
+        <?php } ?>
+        </table>
+    </div>
+    
+    <?php if ($_SESSION['admin']) {?>
+        <div class="register-student">
+            <a href="registerStudent.php">Cadastrar Aluno</a>
+        </div>
+    <?php }?>
         
 <?php
     require_once '../Pages/elements/footer.php';
