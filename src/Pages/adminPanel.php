f4d5728df50e049b9e044942e50e64d420b1d3d4
isaiasaraujosilva <isaiasaraujosilva>
2022-11-04 21:34:20 -0300

    feat: add grid

--- src/Pages/adminPanel.php
+++ src/Pages/adminPanel.php
@@ -33,52 +33,62 @@ if (empty($_SESSION['user']) || empty($_SESSION['password'])) {
     
     include_once 'elements/head.php';
 ?>
-
-    <div class="pagetitle">
-      <h1>Painel do Administrador</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </nav>
-    </div>
-    <div class="d-flex justify-content-around p-2">
-        <div class="cardAdmin col-sm-4 mb-3" style="max-width: 18rem;">
-        <div class="text-center"> 
-            <img class = "rounded" src="../Pages/imgs/people.svg" alt="people" style="width: 4rem;">
-        
-                <div class="card-body">
+    <div class="container">
+      <div class="col">
+        <div class="row mb-3">
+          <div class="pagetitle">
+            <h1>Painel do Administrador</h1>
+            <nav>
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+      </div>
+        <div class="col">
+          <div class="row mb-3">
+            <div class="d-flex justify-content-around p-2">
+              <div class="cardAdmin col-sm-4 mb-3" style="max-width: 18rem;">
+                <div class="text-center"> 
+                  <img class = "rounded" src="../Pages/imgs/people.svg" alt="people" style="width: 4rem;"> 
+                  <div class="card-body">
                     <h5 class="card-title"><a href ="adminPanelAllUser.php">Total de usuários</a></h5>
                     <p class="card-text"><?= $people?></p>
                     <p><p></p></p>
                   </div>
                 </div>
-        </div>
-        <div class="cardAdmin col-sm-4 mb-3" style="max-width: 18rem;">
-        <div class="text-center"> 
-          <img class="rounded" src="../Pages/imgs/teacher.svg" alt="teacher" style="width: 4rem;">
-        
-                <div class="card-body">
-                    <h5 class="card-title">Professores</h5>
-                    <p class="card-text"><?= $teachers?></p>
-                    <p><p></p></p>
-                </div>
-                </div>
-        </div>
-        <div class="cardAdmin col-sm-4 mb-3" style="max-width: 18rem;">
-        <div class="text-center">  
-            <img class = "rounded" src="../Pages/imgs/group-students.svg" alt="group-students" style="width: 4rem;">
-        
-            <div class="card-body">
-                <h5 class="card-title">Estudantes</h5>
+              </div>
+            <div class="cardAdmin col-sm-4 mb-3" style="max-width: 18rem;">
+              <div class="text-center"> 
+              <img class="rounded" src="../Pages/imgs/teacher.svg" alt="teacher" style="width: 4rem;">
+              <div class="card-body">
+                <h5 class="card-title">Professores</h5>
                 <p class="card-text"><?= $teachers?></p>
                 <p><p></p></p>
-                </div> 
+              </div>
             </div>
+          </div>
+        <div class="cardAdmin col-sm-4 mb-3" style="max-width: 18rem;">
+        <div class="text-center">  
+          <img class = "rounded" src="../Pages/imgs/group-students.svg" alt="group-students" style="width: 4rem;">
+          <div class="card-body">
+            <h5 class="card-title">Estudantes</h5>
+            <p class="card-text"><?= $teachers?></p>
+            <p><p></p></p>
+          </div> 
         </div>
+      </div>
     </div>
-  </main>
+  </div>
+  <div class="col">
+    <div class="row">
+      <input class="col mx-sm-3" type="text">
+      <input class = "col" type="text">
+    </div>
+  </div>
+</div>  
 
 <?php 
     include_once 'elements/footer.php';
