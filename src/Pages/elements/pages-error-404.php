f199a1aeebb56acd52713c9398dd5adb423b2ca9
isaiasaraujosilva <isaiasaraujosilva>
2022-11-08 21:55:17 -0300

    feat: remov commit in code

--- src/Pages/elements/pages-error-404.php
+++ src/Pages/elements/pages-error-404.php
@@ -48,10 +48,6 @@
         <input class = "form-control"type="button" value="voltar" onclick="history.go(-1)">
         <img src="../assets/img/not-found.svg" class="img-fluid py-5" alt="Pagina não encontrada">
         <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
         </div>
       </section>
 
