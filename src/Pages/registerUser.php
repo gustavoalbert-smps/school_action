da1f36dc3e10ccd0ef7c68d93117413afc006540
gustavo <gustavoads.millionary@gmail.com>
2022-11-09 11:55:37 -0300

    feature: cards are now part of a form and the add button is an input type.

--- src/Pages/registerUser.php
+++ src/Pages/registerUser.php
@@ -15,51 +15,61 @@ if (empty($_SESSION['user']) || empty($_SESSION['password'])) {
         $connection = ConnectDatabase::connect();
 
         include_once 'elements/head.php';
-        $reponsiveBody;
 ?>
 
         <body>
+            <?php $reponsiveBody = '';?>
             <div class="container">
                 <div class="row">
                     <div class="col-lg-10 mx-auto">
                         <div class="row">
                             <div class="col">
-                                <div class="card">
-                                    <div class="card-body register-card">
-                                        <h4 class="card-title">
-                                            Cadastrar Estudante
-                                        </h4>
-                                        <div class="row align-items-end">
-                                            <div class="col student-img">
-                                                <img id="student-register" class="img-fluid" src="imgs/student-registeruser.svg" alt="student-register-icon">
-                                            </div>
-                                            <div class="col add-card">
-                                                <a id="add-link" href="#"><img id="add-icon" class="img-fluid" src="imgs/add-icon.svg" alt="add-icon"></a>    
+                                <form class="register-user-form" action="registerUser.php" method="POST">
+                                    <div class="card">
+                                        <div class="card-body register-card">
+                                            <h4 class="card-title">
+                                                Cadastrar Estudante
+                                            </h4>
+                                            <div class="row align-items-end">
+                                                <div class="col student-img">
+                                                    <img id="student-register" class="img-fluid" src="imgs/student-registeruser.svg" alt="student-register-icon">
+                                                </div>
+                                                <div class="col add-card">
+                                                    <input class="btn img-fluid" id="add-student" type="submit" placeholder="" name="add-student" value="">
+                                                    <input type="hidden" id="student" name="student" value="student">
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </form>
                             </div>
                             <div class="col">
-                                <div class="card">
-                                    <div class="card-body register-card">
-                                        <h4 class="card-title">
-                                            Cadastrar Professor
-                                        </h4>
-                                        <div class="row align-items-end">
-                                            <div class="col">
-                                                <img id="teacher-register" class="img-fluid" src="imgs/teacher-icon.svg" alt="teacher-register-icon">
-                                            </div>
-                                            <div class="col add-card">
-                                                <a id="add-link" href="#"><img id="add-icon" class="img-fluid" src="imgs/add-icon.svg" alt="add-icon"></a>
+                                <form class="register-user-form" action="registerUser.php" method="POST">
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="card">
+                                                <div class="card-body register-card">
+                                                    <h4 class="card-title">
+                                                        Cadastrar Professor
+                                                    </h4>
+                                                    <div class="row align-items-end">
+                                                        <div class="col">
+                                                            <img id="teacher-register" class="img-fluid" src="imgs/teacher-icon.svg" alt="teacher-register-icon">
+                                                        </div>
+                                                        <div class="col add-card">
+                                                            <input class="btn img-fluid" id="add-teacher" type="submit" name="add-teacher" value="">
+                                                            <input type="hidden" id="teacher" name="teacher" value="teacher">
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
-                        <?php echo var_dump($responsiveBody === 'student');?>
-                        <?php if ($responsiveBody == "student") {
+                        <?php echo var_dump($reponsiveBody === 'student');?>
+                        <?php if ($reponsiveBody == "student") {
                             include_once 'elements/studentRegisterForm.php';
                         }
                         ?>
