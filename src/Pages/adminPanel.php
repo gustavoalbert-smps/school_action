c8f3b7e6f3aeaac443e47f65807e63b91569d8ce
isaiasaraujosilva <isaiasaraujosilva>
2022-11-09 21:35:45 -0300

    feat: change style page

--- src/Pages/adminPanel.php
+++ src/Pages/adminPanel.php
@@ -47,188 +47,86 @@ if (empty($_SESSION['user']) || empty($_SESSION['password'])) {
           </div>
         </div>
       </div>
-      <!--Card menu-->
-      <section class="section dashboard">
+
+      <!-- end page title -->
+
       <div class="row">
+          <div class="col">
+          <div class="card">
+              <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">Usuarios</li>
+                <li class="list-group-item">150</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">Professores</li>
+                <li class="list-group-item">230</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">Estudantes</li>
+                <li class="list-group-item">430</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col">
+          <div class="card">
+              <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">Coordenadores</li>
+                <li class="list-group-item">30</li>
+              </ul>
+            </div>
+          </div>
+      </div>
 
-        <!-- Left side columns -->
-        <div class="col-lg-8">
-          <div class="row">
-            <!-- Sales Card -->
-            <div class="card bg-light mb-3" style="max-width: 18rem;">
-              <div class="card-header text-center">Total de Pessoas Cadastradas</div>
-                <div class="card-body">
-                  <h5 class="card-title text-center">150</h5>
-                </div>
-          </div><!-- End Sales Card -->
-            <div class="card bg-light mb-3 mx-3" style="max-width: 18rem;">
-                <div class="card-header text-center">Total de Professores</div>
-                  <div class="card-body">
-                    <h5 class="card-title text-center">150</h5>
-                  </div>
-                </div>
-              <!-- Revenue Card -->
-           
-            </div><!-- End Revenue Card -->
-
-            <!-- Customers Card -->
-
-
-            </div><!-- End Customers Card -->
-
-            <!-- Reports -->
-
-            <!-- Recent Sales -->
-            <div class="col-12">
-              <div class="card recent-sales overflow-auto">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
-                <div class="card-body">
-                  <h5 class="card-title">Recent Sales <span>| Today</span></h5>
-
-                  <table class="table table-borderless datatable">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Customer</th>
-                        <th scope="col">Product</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Status</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row"><a href="#">#2457</a></th>
-                        <td>Brandon Jacob</td>
-                        <td><a href="#" class="text-primary">At praesentium minu</a></td>
-                        <td>$64</td>
-                        <td><span class="badge bg-success">Approved</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2147</a></th>
-                        <td>Bridie Kessler</td>
-                        <td><a href="#" class="text-primary">Blanditiis dolor omnis similique</a></td>
-                        <td>$47</td>
-                        <td><span class="badge bg-warning">Pending</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2049</a></th>
-                        <td>Ashleigh Langosh</td>
-                        <td><a href="#" class="text-primary">At recusandae consectetur</a></td>
-                        <td>$147</td>
-                        <td><span class="badge bg-success">Approved</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2644</a></th>
-                        <td>Angus Grady</td>
-                        <td><a href="#" class="text-primar">Ut voluptatem id earum et</a></td>
-                        <td>$67</td>
-                        <td><span class="badge bg-danger">Rejected</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2644</a></th>
-                        <td>Raheem Lehner</td>
-                        <td><a href="#" class="text-primary">Sunt similique distinctio</a></td>
-                        <td>$165</td>
-                        <td><span class="badge bg-success">Approved</span></td>
-                      </tr>
-                    </tbody>
-                  </table>
-
-                </div>
-
-              </div>
-            </div><!-- End Recent Sales -->
-
-            <!-- Top Selling -->
-            <div class="col-12">
-              <div class="card top-selling overflow-auto">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
-                <div class="card-body pb-0">
-                  <h5 class="card-title">Top Selling <span>| Today</span></h5>
-
-                  <table class="table table-borderless">
-                    <thead>
-                      <tr>
-                        <th scope="col">Preview</th>
-                        <th scope="col">Product</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Sold</th>
-                        <th scope="col">Revenue</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-1.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Ut inventore ipsa voluptas nulla</a></td>
-                        <td>$64</td>
-                        <td class="fw-bold">124</td>
-                        <td>$5,828</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-2.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Exercitationem similique doloremque</a></td>
-                        <td>$46</td>
-                        <td class="fw-bold">98</td>
-                        <td>$4,508</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-3.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Doloribus nisi exercitationem</a></td>
-                        <td>$59</td>
-                        <td class="fw-bold">74</td>
-                        <td>$4,366</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-4.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Officiis quaerat sint rerum error</a></td>
-                        <td>$32</td>
-                        <td class="fw-bold">63</td>
-                        <td>$2,016</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-5.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Sit unde debitis delectus repellendus</a></td>
-                        <td>$79</td>
-                        <td class="fw-bold">41</td>
-                        <td>$3,239</td>
-                      </tr>
-                    </tbody>
-                  </table>
-
-                </div>
-
-              </div>
-            </div><!-- End Top Selling -->
-
-       <!--end menu-->
-
-     
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <table class="table table-borderless text-center">
+              <thead>
+                <tr class = "bg-success">
+                  <th scope="col">Nome</th>
+                  <th scope="col">Sobrenome</th>
+                  <th scope="col">Categoria</th>
+                  <th scope="col">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class = "text-justify">
+                  <td>Bridie Kessler</td>
+                  <td><a href="#" class="text-primary">Blanditiis dolor omnis similique</a></td>
+                  <td>$47</td>
+                  <td><span class="badge bg-warning">Pending</span></td>
+                </tr>
+                <tr class = "text-justify">
+                  <td>Ashleigh Langosh</td>
+                  <td><a href="#" class="text-primary">At recusandae consectetur</a></td>
+                  <td>$147</td>
+                  <td><span class="badge bg-success">Approved</span></td>
+                </tr>
+                <tr class = "text-justify">
+                  <td>Angus Grady</td>
+                  <td><a href="#" class="text-primar">Ut voluptatem id earum et</a></td>
+                  <td>$67</td>
+                  <td><span class="badge bg-danger">Rejected</span></td>
+                </tr>
+                <tr class = "text-justify">
+                  <td>Raheem Lehner</td>
+                  <td><a href="#" class="text-primary">Sunt similique distinctio</a></td>
+                  <td>$165</td>
+                  <td><span class="badge bg-success">Approved</span></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
 
 <?php 
     include_once 'elements/footer.php';
