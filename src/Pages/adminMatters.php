ef6c08803f58344f3247def7969c824de20f9cad
Gustavo Albert <gustavoads.millionary@gmail.com>
2022-11-20 02:34:25 -0300

    feature: adjustment in resizing options and fixed width.

--- src/Pages/adminMatters.php
+++ src/Pages/adminMatters.php
@@ -38,13 +38,30 @@ if (empty($_SESSION['user']) || empty($_SESSION['password'])) {
                     <div class="col-lg-10 mx-auto">
                         <div class="bg-white rounded-lg shadow-sm p-5">
                             <form class="form" action="adminMatters.php" method="POST">
-                                <div class="mb-3">
+                                <div class="mb-3 checkoption-matter">
                                     <p>Matérias</p>
-                                    <div class="form-check matter">
-                                        <label class="form-check-label shadow-sm" for="math">
-                                            <input class="form-check-input" type="checkbox" value="matemática" name="matter" id="math">
-                                            <span class="label"><i class="bi bi-calculator-fill me-2"></i>Matemática</span>
+                                    <div class="form-check matter d-flex flex-wrap justify-content-between">
+                                        <label class="form-check-label matter-check text-center shadow" for="math">
+                                            <input id="math" class="form-check-input" type="checkbox" value="matemática" name="matter" >
+                                            <span class="label text-break"><i class="bi bi-calculator-fill me-2"></i>Matemática</span>
                                         </label>
+                                        <label for="port" class="form-check-label matter-check text-center shadow">
+                                            <input id="port" class="form-check-input" type="checkbox" value="português" name="matter" >
+                                            <span class="label text-break"><i class="fa-solid fa-language"></i>Português</span>
+                                        </label>
+                                        <label for="geo" class="form-check-label matter-check text-center shadow">
+                                            <input id="geo" class="form-check-input" type="checkbox" value="geografia" name="matter">
+                                            <span class="label text-break">Geografia</span>
+                                        </label>
+                                        <label for="hist" class="form-check-label matter-check text-center shadow">
+                                            <input id="hist" class="form-check-input" type="checkbox" value="história" name="matter">
+                                            <span class="label text-break">História</span>
+                                        </label>
+                                        <label for="chem" class="form-check-label matter-check text-center shadow">
+                                            <input id="chem" class="form-check-input" type="checkbox" value="química" name="matter">
+                                            <span class="label text-break">Química</span>
+                                        </label>
+                                        <!-- label.form-check-label.matter-check.text-center.shadow-sm>input.form-check-input+span.label -->
                                     </div>
                                 </div>
                             </form>
